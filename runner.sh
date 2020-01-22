459f2f59546c43e58e20ffca8a2c484e09c307d8
figbux <celenkah@itu.edu.tr>
2020-01-22 11:15:08 +0300

    FULL-AUTOMATIC MODIFIED SCRIPT EXECUTER v -2

--- runner.sh
+++ runner.sh
@@ -5,21 +5,25 @@
 # Run this in a seperate terminal to automate running your
 # script/code/etc. whenever you save it
 #
-# ./runner.sh <command-to-run> <file_to_track>
+# ./runner.sh <command-to-run> <file_to_track> <arguments>
+#
+# 2nd arg is always expected the file to be tracked.
 #
 # Examples:
-#       ./runner.sh "python3" "test.py"
-#       ./runner.sh "bash" "script.sh"
-#       ./runner.sh "python3" "test.py"
-#       ./runner.sh "nasm" "code.asm"                    # nifty live assembler
+#       ./runner.sh python3 test.py -t=all --foo=bar
+#       ./runner.sh bash script.sh
+#       ./runner.sh nasm code.asm                        # nifty live assembler
 #       ./runner.sh "gcc main.c && ./a.out" "main.c"     # nifty live compiler
 #
 # Autosave command for vim :autocmd TextChanged,TextChangedI <buffer> silent write
+
 TRIAL=0
 
 doit() {
-        $1 $2
+        # run
+        $@
 
+        # print
         TRIAL=$(($TRIAL+1))
         COLS=$(tput cols)
         COLOR=$(tput setaf $(($TRIAL % 10 + 1)))
@@ -33,11 +37,11 @@ doit() {
         printf "\n"
 }
 
-# run it at the beginning
-doit $1 $2
+# run it in the beginning once
+doit $@
 
 # run it on file modification
 while (inotifywait -q -e close_write $2 > /dev/null)
 do
-        doit $1 $2
+        doit $@
 done
